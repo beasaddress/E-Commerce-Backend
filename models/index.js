41cdf66afd4093acc9beed19d1840685d5e7887b
Beatriz Sandoval <beasaddress@gmail.com>
2023-07-06 01:00:47 -0700

    added a through option in the belongstoMany association so that product can belongtomany tags through ProductTag table. database is able to seed just fine with node seeds/index.js command. npm start loads the application on the correct port. All get requests work just fine. Going to get out the other routes

--- models/index.js
+++ models/index.js
@@ -16,6 +16,7 @@ Category.hasMany(Product, {
 });
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
+  through: 'ProductTag',
   foreignKey: 'tag_id',
 });
 // Tags belongToMany Products (through ProductTag)
